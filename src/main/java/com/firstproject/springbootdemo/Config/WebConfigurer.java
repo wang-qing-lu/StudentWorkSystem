9ac43bd195d539c2da7296d0b4e3e2f5220acb8b
wangqinglu <wangqinglu@countrygarden.com.cn>
2020-05-06 10:56:16 +0800

    修改拦截器配置

--- src/main/java/com/firstproject/springbootdemo/Config/WebConfigurer.java
+++ src/main/java/com/firstproject/springbootdemo/Config/WebConfigurer.java
@@ -11,24 +11,24 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfigurer implements WebMvcConfigurer{
 
-    @Autowired
-    private LoginInterceptor loginInterceptor;
-    /**
-     * 此方法用来配置静态资源，例如css，js，html等
-     * @param registry
-     */
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    }
-
-    /**
-     * 这个方法用来注册拦截器，我们自己写好的拦截器需要通过这里添加注册才能生效
-     * @param registry
-     */
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        // addPathPatterns("/**") 表示拦截所有的请求，
-        // excludePathPatterns("/login", "/register") 表示除了登陆与注册之外，因为登陆注册不需要登陆也可以访问
-        registry.addInterceptor(loginInterceptor).addPathPatterns("/**").excludePathPatterns("/login", "/index" ,"/replacePassword");
-    }
+//    @Autowired
+//    private LoginInterceptor loginInterceptor;
+//    /**
+//     * 此方法用来配置静态资源，例如css，js，html等
+//     * @param registry
+//     */
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//    }
+//
+//    /**
+//     * 这个方法用来注册拦截器，我们自己写好的拦截器需要通过这里添加注册才能生效
+//     * @param registry
+//     */
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        // addPathPatterns("/**") 表示拦截所有的请求，
+//        // excludePathPatterns("/login", "/register") 表示除了登陆与注册之外，因为登陆注册不需要登陆也可以访问
+//        registry.addInterceptor(loginInterceptor).addPathPatterns("/**").excludePathPatterns("/login", "/index" ,"/replacePassword");
+//    }
 }
